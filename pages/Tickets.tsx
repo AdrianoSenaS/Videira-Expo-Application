52cf061f338034594432b8dd3177bd91da204db4
AdrianoSenaS <adryanosenaislva@gmail.com>
2025-01-08 13:26:57 -0300

    Inciando Tickets

---
+++ pages/Tickets.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { useTheme } from '../themes/ThemeContext';
+import { lightTheme, darkTheme } from '../themes/Themes';
+
+const Ticket: React.FC = ()=>{
+
+    return (
+        <View>
+
+        </View>
+    )
+
+}
+
+export default Ticket
